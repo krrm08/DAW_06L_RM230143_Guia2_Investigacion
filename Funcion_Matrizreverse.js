86685e0645478234b8db2b1de6cc17c363901aea
krrm08 <kevinroberto026@gmail.com>
2023-08-20 21:18:24 -0600

    Guia2 Investigación complementaria

---
+++ Funcion_Matrizreverse.js
@@ -0,0 +1,9 @@
+// Creamos un array
+let numeros = [1, 2, 3, 4, 5];
+
+console.log("Array original:", numeros);
+
+// Usamos el método reverse() para invertir el orden de los elementos
+numeros.reverse();
+
+console.log("Array después de reverse:", numeros);
---
+++ Metodos_push_pop.js
@@ -0,0 +1,16 @@
+let frutas = [];
+
+// Utilizamos push() para agregar elementos al final del array
+frutas.push("Manzana");
+frutas.push("Banana");
+frutas.push("Naranja");
+frutas.push("Fresa")
+
+console.log("Arreglo después de push:", frutas);
+
+let removedfrutas = frutas.pop();
+
+console.log("Array después de pop:", frutas);
+
+console.log("Se ha borrado el elemento:", removedfrutas);
+
